2b22512d540fdd49e6d00a9c7b4f20d04c4b713f
Annika Clarke <annika.clarke@gmail.com>
2015-03-31 16:05:47 -0400

    Update parse_requirements for pip

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import os
-from setuptools import setup
-from pip.req import parse_requirements
+from setuptools import setup, find_packages
+import pip
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('requirements.txt', pip.download.PipSession())
+install_reqs = pip.req.parse_requirements('requirements.txt', pip.download.PipSession())
 
 # reqs is a list of requirement
 reqs = [str(ir.req) for ir in install_reqs]
